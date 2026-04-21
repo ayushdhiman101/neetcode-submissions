ee0803a3c5fdf6153a51f52f07cc69ad29ec2717
Ayush Dhiman <61506157+ayushdhiman101@users.noreply.github.com>
2026-04-21 23:48:09 +0530

    Add: permutations - submission-1

---
+++ Data Structures & Algorithms/permutations/submission-1.java
@@ -0,0 +1,31 @@
+class Solution {
+    List<List<Integer>> ans = new ArrayList<>();
+    public List<List<Integer>> permute(int[] nums) {
+        if(nums.length == 0) return ans;
+        permute2(nums, new ArrayList<>(), 0);
+        return ans;
+        
+    }
+    public void permute2(int[] nums, List<Integer> temp, int idx) {
+
+        if(idx == nums.length) {
+            List<Integer> list = new ArrayList<>();
+            for(int i: nums) list.add(i);
+            // if(!ans.contains(list)) 
+                ans.add(list);
+            return;
+        }
+        for(int i = idx; i < nums.length; i++) {
+            swap(nums, idx, i);
+            permute2(nums, temp, idx + 1);
+            swap(nums, i, idx);
+            // permute2(nums, temp, idx - 1);
+        }
+        return;
+    }
+    public void swap(int[] nums, int idx, int i) {
+        int temp = nums[idx];
+        nums[idx] = nums[i];
+        nums[i] = temp;
+    }
+ }
